7ee3620bdbb2ab236e77327a50953d4e93af8042
alexsykes8 <65182304+alexsykes8@users.noreply.github.com>
2024-12-05 18:54:35 +0000

    5/12

--- Main/entry.py
+++ Main/entry.py
@@ -3,7 +3,7 @@ from list_management import list_management
 
 def main():
     currentUser = user()
-    currentUser.login("test")
+    currentUser.login("test2")
 
 if __name__ == "__main__":
     main()
